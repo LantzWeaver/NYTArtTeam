4fb895d773b62bbfd8c051e936b16d881a9cf1fc
Tanner Smith <smithtannera@wifi_stu-10-38-43-55.uncc.edu>
2017-07-06 21:47:48 -0400

    NYT Files

--- assets/javascript/NYTProject.js
+++ assets/javascript/NYTProject.js
@@ -13,9 +13,9 @@ filterList, highlighter, pageResults, articleStats, filterStats;
 
         // store user input
         search = $("#searchTerms").val().trim();
-        dateStart = $("#dateStart").val().trim();
-        dateEnd = $("#dateEnd").val().trim();
-        pageResults = $("#pageResults").val().trim();
+        //dateStart = $("#dateStart").val().trim();
+        //dateEnd = $("#dateEnd").val().trim();
+        //pageResults = $("#pageResults").val().trim();
         //var movie = $("#highlighter").val().trim();
         //var movie = $("#articleStats").val().trim();
        
@@ -24,19 +24,19 @@ filterList, highlighter, pageResults, articleStats, filterStats;
 
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
+queryURL += '?' + $.param({
 
-  'api-key': "de8d86840824415490757a65af5f2436"
-  'q': search
-  'fq': filterSearch
-  'begin_date': dateStart
-  'end_date': dateEnd
-  'sort': sortBy
-  'fl': filterList
-  'hl': highlighter
-  'page': pageResults
-  'facet_field': articleStats
-  'facet_filter': filterStats
+  'api-key': "de8d86840824415490757a65af5f2436",
+  'q': search,
+  //'fq': filterSearch,
+  //'begin_date': dateStart,
+  //'end_date': dateEnd,
+  //'sort': sortBy,
+  //'fl': filterList,
+  //'hl': highlighter,
+  //'page': pageResults,
+  //'facet_field': articleStats,
+  //'facet_filter': filterStats
 
 });
 
@@ -47,13 +47,19 @@ function jax(){
       method: 'GET'
     }).done(function(response) {
 
-      var searchResult = response;
-      console.log(searchResult)
+      var searchResult = response.response.docs;
+      console.log(searchResult);
+      console.log(queryURL);
 
     for (i=0; i < searchResult.length; i++){
 
-      var newsDiv = $('<div>');
-      $('newsDiv').append(searchResult[i]);
+      var searchResultClean = searchResult[i];
+      var searchResultExtraClean = searchResultClean.headline.main;
+      console.log(searchResultExtraClean);
+
+      $('#newsResult').append(searchResultExtraClean);
+      
+      
 
     };
 
@@ -63,6 +69,7 @@ function jax(){
  jax();
 });
 
-}
+})
+
 
 
